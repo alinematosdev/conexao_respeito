6a9cbc9fc67dfbdda586219e7e3b36f7f91b98a8
alinematosdev <alinemdev@gmail.com>
2024-05-14 14:29:56 -0300

    modifying tabs routes

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -34,14 +34,14 @@ export const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/home',
+        redirectTo: '/tabs/login',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/home',
+    redirectTo: '/login',
     pathMatch: 'full',
   },
 
